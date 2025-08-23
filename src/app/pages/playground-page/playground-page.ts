51e0b3a9d6ec2bda5836adaf9b2651d2a14f0e4a
dentiman <den.timanovskkiy@gmail.com>
2025-08-23 11:14:21 +0300

    Refactor Playground page layout and styles to improve grid structure and visual consistency, replacing old list items with a new grid-based design and updating associated CSS classes.

--- src/app/pages/playground-page/playground-page.ts
+++ src/app/pages/playground-page/playground-page.ts
@@ -6,121 +6,60 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div class="w-100">
-      <ol >
-        <li class="bg-conic/decreasing from-violet-700 via-lime-300 to-violet-700" ></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+    <div class="w-15">
+      <ol class="grid grid-cols-34 grid-rows-21  list-none m-0 p-0">
+        <li class="aspect-square overflow-hidden relative border-r-2  border-r-transparent" style="grid-area: 1 / 1 / 22 / 22;">
+          <div class="absolute inset-0  rounded-full border-3  border-gray-400 bg-gray-300   scale-[2]  translate-x-1/2 translate-y-1/2 overflow-hidden">
+
+          </div>
+        </li>
+        <li class="aspect-square overflow-hidden relative border-b-1 border-b-transparent" style="grid-area: 1 / 22 / 23 / 35;">
+          <div class="absolute inset-0 rounded-full  border-3  border-gray-300 bg-gray-400 scale-[2] -translate-x-1/2 translate-y-1/2">
+
+          </div>
+        </li>
+        <li class="aspect-square overflow-hidden relative border-l-1 border-l-transparent" style="grid-area: 14 / 27 / 22 / 35;">
+          <div class="absolute inset-0 rounded-full border-3  border-gray-400 bg-gray-300 scale-[2] -translate-x-1/2 -translate-y-1/2"></div>
+        </li>
+        <li class="aspect-square overflow-hidden relative border-t-1 border-t-transparent" style="grid-area: 17 / 22 / 22 / 27;">
+          <div class="absolute inset-0 rounded-full border-3  border-gray-300 bg-gray-400 scale-[2] translate-x-1/2 -translate-y-1/2"></div>
+        </li>
+        <li class="aspect-square overflow-hidden relative " style="grid-area: 14 / 22 / 17 / 25;">
+          <div class="absolute inset-0 rounded-full border-3 border-gray-400 bg-gray-300 scale-[2] translate-x-1/2 translate-y-1/2"></div>
+        </li>
+        <li class="aspect-square overflow-hidden relative " style="grid-area: 14 / 25 / 17 / 27;">
+          <div class="absolute inset-0 rounded-full border-3 border-gray-400 bg-gray-300 scale-[2] -translate-x-1/2 translate-y-1/2"></div>
+        </li>
+        <li class="aspect-square overflow-hidden relative " style="grid-area: 16 / 26 / 17 / 27;">
+          <div class="absolute inset-0 rounded-full border-3 border-gray-400 bg-gray-300 scale-[2] -translate-x-1/2 -translate-y-1/2"></div>
+        </li>
+        <li class="aspect-square overflow-hidden relative " style="grid-area: 16 / 25 / 17 / 26;">
+          <div class="absolute inset-0 rounded-full border-3 border-gray-400 bg-gray-300 scale-[2] translate-x-1/2 -translate-y-1/2"></div>
+        </li>
       </ol>
     </div>
-
-    <div is="circle" class="size-18 rounded-full bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90%"></div>
 
 
   `,
   styles: [`
-    ol {
-      all: unset;
-      display: grid;
+    .grid-cols-34 {
       grid-template-columns: repeat(34, 1fr);
+    }
+
+    .grid-rows-21 {
       grid-template-rows: repeat(21, 1fr);
-      list-style: none;
-      background: transparent;
+    }
 
-      li {
-        aspect-ratio: 1 / 1;
-        background: var(--bg);
-        grid-area: var(--ga);
-        overflow: hidden;
-        position: relative;
+    .w-18 {
+      width: 4.5rem;
+    }
 
-        &::after {
-          aspect-ratio: 1 / 1;
-
-          border-radius: 50%;
-          content: '';
-          display: block;
-          inset: 0;
-          position: absolute;
-          scale: 2;
-          translate: var(--tl);
-        }
-
-        &:nth-of-type(1) {
-          --ga: 1 / 1 / 22 / 22;
-          --tl: 50% 50%;
-
-          &::after {
-            background-image: linear-gradient(to right top, #f70606, #fa4c00, #fc6f00, #fc8d00, #fba700);
-          }
-        }
-        &:nth-of-type(2) {
-          --ga: 1 / 22 / 23 / 35;
-          --tl: -50% 50%;
-
-          &::after {
-            background-image: linear-gradient(to right bottom, #fc6f00, #ff9300, #ffb600, #ffd900, #f7fb00);
-          }
-
-        }
-        &:nth-of-type(3) {
-          --ga: 14 / 27 / 22 / 35;
-          --tl: -50% -50%;
-
-          &::after {
-            background-image: linear-gradient(to bottom, #ff9300, #e0a600, #b7b600, #82c400, #11cf03);
-          }
-
-        }
-        &:nth-of-type(4) {
-          --ga: 17 / 22 / 22 / 27;
-          --tl: 50% -50%;
-
-          &::after {
-            background-image: linear-gradient(to left, #11cf03, #00d560, #00d892, #00dab8, #01d9d2);
-          }
+    .h-18 {
+      height: 4.5rem;
+    }
 
 
-        }
-        &:nth-of-type(5) {
-          --ga: 14 / 22 / 17 / 25;
-          --tl: 50% 50%;
-
-          &::after {
-            background-image: linear-gradient(to right top, #f70606, #fa4c00, #fc6f00, #fc8d00, #fba700);
-          }
-        }
-        &:nth-of-type(6) {
-          --ga: 14 / 25 / 17 / 27;
-          --tl: -50% 50%;
-
-          &::after {
-            background-image: linear-gradient(to right top, #f70606, #fa4c00, #fc6f00, #fc8d00, #fba700);
-          }
-        }
-        &:nth-of-type(7) {
-          --ga: 16 / 26 / 17 / 27;
-          --tl: -50% -50%;
-
-          &::after {
-            background-image: linear-gradient(to right top, #f70606, #fa4c00, #fc6f00, #fc8d00, #fba700);
-          }
-        }
-        &:nth-of-type(8) {
-          --ga: 16 / 25 / 17 / 26;
-          --tl: 50% -50%;
-
-          &::after {
-            background-image: linear-gradient(to right top, #f70606, #fa4c00, #fc6f00, #fc8d00, #fba700);
-          }
-        }
-      }
-    }`]
+  `]
 })
 export class PlaygroundPageComponent {
 }
